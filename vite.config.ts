1e09fdbf5d65fabebadef500025b2a89829869ec
Densingh-123 <ddensingh19@gmail.com>
2025-10-26 19:14:20 +0530

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       server: {
         port: 3000,
@@ -16,8 +19,12 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, 'src'),
         }
+      },
+      build: {
+        target: 'esnext',
+        sourcemap: false
       }
     };
 });
